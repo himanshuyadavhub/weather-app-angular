ffb73d731e98bba34ca6a73b12ba32abbf6dbe37
Himanshu <himanshuyadav9536w@gmail.com>
2026-04-08 09:05:51 +0530

    First Commit

---
+++ src/app/weather-dashboard/weather-dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-dashboard',
+  imports: [],
+  templateUrl: './weather-dashboard.html',
+  styleUrls: ['./weather-dashboard.css'],
+  standalone: true
+})
+export class WeatherDashboard {}
